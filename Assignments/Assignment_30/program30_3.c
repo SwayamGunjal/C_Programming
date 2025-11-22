5e60829f9ccd03f8b68f1c144ca2711554808abd
Swayam Gunjal <swayamgunjal2@gmail.com>
2025-11-22 16:11:26 +0530

    Add Assignment_30 solutions (Q1-Q5)

---
+++ Assignments/Assignment_30/program30_3.c
@@ -0,0 +1,74 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//      Function name : Pattern
+//      Description :   It prints a square matrix where the diagonal shows '$' and all other positions show '*'.
+//      Input :         Integer
+//      Output :        Void
+//      Author :        Swayam Satish Gunjal
+//      Date :          21/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+ 
+Input :     
+
+    iRow :  5
+    iCol :  5
+
+Output :    
+
+    $   *   *   *   *
+    *   $   *   *   *
+    *   *   $   *   *
+    *   *   *   $   *
+    *   *   *   *   $
+
+ */
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+        
+    if(iRow != iCol)
+    {
+        printf("Invalid input.\n");
+        printf("Row number and column number should be same.\n");
+
+        return;
+    }
+    for(i = 1; i <= iRow; i++)  
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(i == j)
+            {
+                printf("$\t");
+            }
+            else if(i >= j)
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+        
+    printf("Enter number of Rows and Columns : \n");
+    scanf("%d %d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+
+    return 0;
+}

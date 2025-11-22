e56f3e5e08d4dd45c78e9dbfb17e8031e9626e9d
Swayam Gunjal <swayamgunjal2@gmail.com>
2025-11-22 15:32:56 +0530

    Add Assignment_27 solutions (Q1-Q5)

---
+++ Assignments/Assignment_27/program27_3.c
@@ -0,0 +1,54 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//      Function name : Pattern
+//      Description :   It prints a number matrix where each row contains 1 to the given number of columns.
+//      Input :         Integer
+//      Output :        Void
+//      Author :        Swayam Satish Gunjal
+//      Date :          21/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+ 
+Input :     
+
+    iRow :  3
+    iCol :  5
+
+Output :    
+
+    5   4   3   2   1
+    5   4   3   2   1
+    5   4   3   2   1
+
+ */
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = iCol; j >= 1; j--)
+        {
+            printf("%d\t",j);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+        
+    printf("Enter number of Rows and Columns : \n");
+    scanf("%d %d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+
+    return 0;
+}
---
+++ Assignments/Assignment_27/program27_4.c
@@ -0,0 +1,62 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//      Function name : Pattern
+//      Description :   It prints a matrix where '*' and '#' alternate column-wise.
+//      Input :         Integer
+//      Output :        Void
+//      Author :        Swayam Satish Gunjal
+//      Date :          21/11/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+ 
+Input :     
+
+    iRow :  3
+    iCol :  4
+
+Output :    
+
+    *   #   *   #
+    *   #   *   #
+    *   #   *   #
+
+ */
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if((j % 2) == 0)
+            {
+                printf("#\t");
+            }
+            else 
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+        
+    printf("Enter number of Rows and Columns : \n");
+    scanf("%d %d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+
+    return 0;
+}

eb74c665221fe15b2b3b7dd5c84a0b4de66cb18c
Swayam Gunjal <swayamgunjal2@gmail.com>
2025-12-16 03:45:26 +0530

    Add Assignment_43 solutions (Q1-Q5)

---
+++ Assignments/Assignment_43/program43_1.c
@@ -0,0 +1,202 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//      Function name : Mult
+//      Description :   Calculate the product of digits of the given number using recursion.
+//      Input :         Integer
+//      Output :        Integer
+//      Author :        Swayam Satish Gunjal
+//      Date :          16/12/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+void InsertFirst(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+    if (*first == NULL)                     //  LL is empty
+    {
+        *first = newn;   
+    }
+    else                                    //  LL contains atleast 1 node
+    {
+        newn -> next = (*first);
+        (*first) = newn;
+    }
+    
+}
+
+void InsertLast(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+
+    if (*first == NULL)                     //  LL is empty
+    {
+        (*first) = newn;   
+    }
+    else                                    //  LL contains atleast 1 node
+    {
+        temp = *first;
+
+        while (temp -> next != NULL)
+        {
+            temp = temp -> next;
+        }
+        
+        temp -> next = newn;
+    }
+    
+}
+
+void Display(PNODE first)
+{
+    while (first != NULL)
+    {
+        printf("| %d |->",first -> data);
+        first = first -> next;
+    }
+    printf("NULL \n");
+    
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+
+    while (first != NULL)
+    {
+        iCount++;
+        first = first -> next;
+    }
+
+    return iCount;
+    
+}
+
+void DeleteFirst(PPNODE first)
+{
+    PNODE temp = NULL;
+
+    if ((*first) == NULL)                   //  Case 1
+    {
+        return;
+    }
+    else if ((*first) -> next == NULL)      //  Case 2
+    {
+        free(*first);
+        *first = NULL;
+    }
+    else                                    //  Case 3
+    {
+        temp = *first;
+        *first = (*first) -> next;
+        free(temp);
+        
+    }  
+}
+
+void DeleteLast(PPNODE first)
+{
+    PNODE temp = NULL;
+
+    if ((*first) == NULL)                   //  Case 1
+    {
+        return;
+    }
+    else if ((*first) -> next == NULL)      //  Case 2
+    {
+        free(*first);
+        *first = NULL;
+    }
+    else                                    //  Case 3
+    {
+        temp = *first;
+
+        while (temp -> next -> next != NULL)
+        {
+            temp = temp -> next;
+        }
+
+        free(temp -> next);
+        temp -> next = NULL;
+        
+    }  
+}
+
+void DisplayPerfect(PNODE head)
+{
+    int iSum = 0, iNo = 0, iCnt = 0;
+
+    while (head != NULL)
+    {
+        iNo = head -> data;
+        iSum = 0;
+
+        for (iCnt = 1; iCnt <= iNo/2; iCnt++)
+        {
+            if ((iNo % iCnt) == 0)
+            {
+                iSum = iSum + iCnt;
+            }
+        }
+        
+        if (iNo == iSum)
+        {
+            printf("%d\t", iNo);
+        }
+
+        head = head -> next;
+        
+    }
+    
+    
+}
+
+int main()
+{
+    int iRet = 0, iRes = 0;
+
+    PNODE head = NULL;
+
+
+    InsertLast(&head,11);
+    InsertLast(&head,28);
+    InsertLast(&head,17);
+    InsertLast(&head,41);
+    InsertLast(&head,6);
+    InsertLast(&head,89);
+    
+    Display(head);
+    iRet = Count(head);
+
+    printf("Number of nodes are : %d\n",iRet);
+
+    DisplayPerfect(head);
+
+    
+    return 0;
+}
---
+++ Assignments/Assignment_43/program43_5.c
@@ -0,0 +1,203 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//      Function name : SumDigit
+//      Description :   Display the sum of digits of each element from the singly linked list.
+//      Input :         PNODE
+//      Output :        Void
+//      Author :        Swayam Satish Gunjal
+//      Date :          16/12/2025
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+
+Input structure : | 110 |->| 230 |->| 20 |->| 240 |->| 640 |->NULL
+
+Output          : 2       5       2       6       10
+
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+void InsertFirst(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+    if (*first == NULL)                     //  LL is empty
+    {
+        *first = newn;   
+    }
+    else                                    //  LL contains atleast 1 node
+    {
+        newn -> next = (*first);
+        (*first) = newn;
+    }
+    
+}
+
+void InsertLast(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+
+    if (*first == NULL)                     //  LL is empty
+    {
+        (*first) = newn;   
+    }
+    else                                    //  LL contains atleast 1 node
+    {
+        temp = *first;
+
+        while (temp -> next != NULL)
+        {
+            temp = temp -> next;
+        }
+        
+        temp -> next = newn;
+    }
+    
+}
+
+void Display(PNODE first)
+{
+    while (first != NULL)
+    {
+        printf("| %d |->",first -> data);
+        first = first -> next;
+    }
+    printf("NULL \n");
+    
+}
+
+int Count(PNODE first)
+{
+    int iCount = 0;
+
+    while (first != NULL)
+    {
+        iCount++;
+        first = first -> next;
+    }
+
+    return iCount;
+    
+}
+
+void DeleteFirst(PPNODE first)
+{
+    PNODE temp = NULL;
+
+    if ((*first) == NULL)                   //  Case 1
+    {
+        return;
+    }
+    else if ((*first) -> next == NULL)      //  Case 2
+    {
+        free(*first);
+        *first = NULL;
+    }
+    else                                    //  Case 3
+    {
+        temp = *first;
+        *first = (*first) -> next;
+        free(temp);
+        
+    }  
+}
+
+void DeleteLast(PPNODE first)
+{
+    PNODE temp = NULL;
+
+    if ((*first) == NULL)                   //  Case 1
+    {
+        return;
+    }
+    else if ((*first) -> next == NULL)      //  Case 2
+    {
+        free(*first);
+        *first = NULL;
+    }
+    else                                    //  Case 3
+    {
+        temp = *first;
+
+        while (temp -> next -> next != NULL)
+        {
+            temp = temp -> next;
+        }
+
+        free(temp -> next);
+        temp -> next = NULL;
+        
+    }  
+}
+
+void SumDigit(PNODE head)
+{
+    int iSum = 0, iDigit = 0, iNo = 0;
+
+    while (head != NULL)
+    {
+        iNo = head -> data;
+        iSum = 0;
+
+        while (iNo != 0)
+        {
+            iDigit = iNo % 10;
+            iSum = iSum + iDigit;
+            iNo = iNo / 10;
+        
+        }
+        
+        printf("%d\t",iSum);
+           
+        head = head -> next;
+    }
+    printf("\n");
+    
+}
+
+int main()
+{
+    int iRet = 0, iRes = 0;
+
+    PNODE head = NULL;
+
+
+    InsertLast(&head,110);
+    InsertLast(&head,230);
+    InsertLast(&head,20);
+    InsertLast(&head,240);
+    InsertLast(&head,640);
+    
+    Display(head);
+    iRet = Count(head);
+
+    printf("Number of nodes are : %d\n",iRet);
+
+    SumDigit(head);
+
+    return 0;
+}

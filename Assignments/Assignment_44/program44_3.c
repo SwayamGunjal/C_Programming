0a828b54ee1fa0decb6c7c478e42008396df4fea
Swayam Gunjal <swayamgunjal2@gmail.com>
2026-01-02 14:26:47 +0530

    Add Assignment_44 solutions (Q1-Q5)

---
+++ Assignments/Assignment_44/program44_3.c
@@ -0,0 +1,108 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//      Function name : Search
+//      Description :   Checks if the element is present or not.
+//      Input :         PNODE
+//      Output :        BOOL
+//      Author :        Swayam Satish Gunjal
+//      Date :          01/01/2026
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+void InsertFirst(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+
+    if (*first == NULL)                     //  LL is empty
+    {
+        *first = newn;   
+    }
+    else                                    //  LL contains atleast 1 node
+    {
+        newn -> next = (*first);
+        (*first) = newn;
+    }
+    
+}
+
+void InsertLast(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+
+    if (*first == NULL)                     //  LL is empty
+    {
+        (*first) = newn;   
+    }
+    else                                    //  LL contains atleast 1 node
+    {
+        temp = *first;
+
+        while (temp -> next != NULL)
+        {
+            temp = temp -> next;
+        }
+        
+        temp -> next = newn;
+    }
+    
+}
+
+int CountEven(PNODE head)
+{
+    int EvenCnt = 0;
+
+    while (head != NULL)
+    {
+        if (((head -> data) % 2) == 0)
+        {
+            EvenCnt++;
+        }   
+        head = head -> next;
+    }
+    return EvenCnt;
+}
+
+int main()
+{
+    int iRet = 0;
+
+    PNODE head = NULL;
+
+    InsertFirst(&head,10);
+    InsertFirst(&head,21);
+    InsertFirst(&head,51);
+    InsertLast(&head,101);
+    InsertLast(&head,102);
+    InsertLast(&head,103);
+
+    iRet = CountEven(head);
+
+    printf("Number of nodes containing even values : %d",iRet);
+
+    return 0;
+}
---
+++ Assignments/Assignment_44/program44_4.c
@@ -0,0 +1,110 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//      Function name : Frequency
+//      Description :   Returns frequency of given number.
+//      Input :         PNODE
+//      Output :        Integer
+//      Author :        Swayam Satish Gunjal
+//      Date :          01/01/2026
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+void InsertFirst(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+
+    if (*first == NULL)                     //  LL is empty
+    {
+        *first = newn;   
+    }
+    else                                    //  LL contains atleast 1 node
+    {
+        newn -> next = (*first);
+        (*first) = newn;
+    }
+    
+}
+
+void InsertLast(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+
+    if (*first == NULL)                     //  LL is empty
+    {
+        (*first) = newn;   
+    }
+    else                                    //  LL contains atleast 1 node
+    {
+        temp = *first;
+
+        while (temp -> next != NULL)
+        {
+            temp = temp -> next;
+        }
+        
+        temp -> next = newn;
+    }
+    
+}
+
+int Frequency(PNODE head, int no)
+{
+    int iFreq = 0;
+
+    while (head != NULL)
+    {
+        if ((head -> data) == no)
+        {
+            iFreq++;
+        }   
+        head = head -> next;
+    }
+    return iFreq;
+}
+
+int main()
+{
+    int iRet = 0;
+
+    PNODE head = NULL;
+
+    InsertFirst(&head,10);
+    InsertFirst(&head,21);
+    InsertFirst(&head,51);
+    InsertFirst(&head,67);
+    InsertLast(&head,101);
+    InsertLast(&head,102);
+    InsertLast(&head,67);
+    InsertLast(&head,103);
+
+    iRet = Frequency(head, 67);
+
+    printf("Frequency of given number : %d",iRet);
+
+    return 0;
+}

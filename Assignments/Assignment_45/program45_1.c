467df5b11bcd40c8bd946aa80716931d54db396f
Swayam Gunjal <swayamgunjal2@gmail.com>
2026-01-02 16:48:43 +0530

    Add Assignment_45 solutions (Q1-Q5)

---
+++ Assignments/Assignment_45/program45_1.c
@@ -0,0 +1,104 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//      Function name : DisplayOdd
+//      Description :   Displays only odd elements in the LinkedList.
+//      Input :         PNODE
+//      Output :        Void
+//      Author :        Swayam Satish Gunjal
+//      Date :          01/01/2026
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+void InsertFirst(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+
+    if (*first == NULL)                     //  LL is empty
+    {
+        *first = newn;   
+    }
+    else                                    //  LL contains atleast 1 node
+    {
+        newn -> next = (*first);
+        (*first) = newn;
+    }
+    
+}
+
+void InsertLast(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+
+    if (*first == NULL)                     //  LL is empty
+    {
+        (*first) = newn;   
+    }
+    else                                    //  LL contains atleast 1 node
+    {
+        temp = *first;
+
+        while (temp -> next != NULL)
+        {
+            temp = temp -> next;
+        }
+        
+        temp -> next = newn;
+    }
+    
+}
+
+void DisplayOdd(PNODE head)
+{
+    while (head != NULL)
+    {
+        if (((head -> data) % 2) != 0)
+        {
+            printf("| %d |->",head->data);
+        }   
+        head = head -> next;
+    }
+    printf("NULL\n");
+}
+
+int main()
+{
+    PNODE head = NULL;
+
+    InsertFirst(&head,10);
+    InsertFirst(&head,21);
+    InsertFirst(&head,51);
+    InsertFirst(&head,68);
+    InsertLast(&head,101);
+    InsertLast(&head,102);
+    InsertLast(&head,62);
+    InsertLast(&head,103);
+
+    DisplayOdd(head);
+
+    return 0;
+}
---
+++ Assignments/Assignment_45/program45_2.c
@@ -0,0 +1,122 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//      Function name : FirstOccurrence
+//      Description :   Returns the position of the first occurrence of the given element;
+//      Input :         PNODE
+//      Output :        Integer
+//      Author :        Swayam Satish Gunjal
+//      Date :          01/01/2026
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#pragma pack(1)
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+void InsertFirst(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+
+    if (*first == NULL)                     //  LL is empty
+    {
+        *first = newn;   
+    }
+    else                                    //  LL contains atleast 1 node
+    {
+        newn -> next = (*first);
+        (*first) = newn;
+    }
+    
+}
+
+void InsertLast(PPNODE first, int no)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = no;
+    newn -> next = NULL;
+
+    if (*first == NULL)                     //  LL is empty
+    {
+        (*first) = newn;   
+    }
+    else                                    //  LL contains atleast 1 node
+    {
+        temp = *first;
+
+        while (temp -> next != NULL)
+        {
+            temp = temp -> next;
+        }
+        
+        temp -> next = newn;
+    }
+    
+}
+
+int FirstOccurrence(PNODE Head, int No)
+{
+    int iPos = 1;
+
+    while (Head != NULL)
+    {
+        if ((Head -> data) == No)
+        {
+            return iPos;
+        }   
+        iPos++;
+        Head = Head -> next;
+    }
+    return -1;
+}
+
+void Display(PNODE first)
+{
+    while (first != NULL)
+    {
+        printf("| %d |->",first -> data);
+        first = first -> next;
+    }
+    printf("NULL \n");
+    
+}
+int main()
+{
+    int iRet = 0;
+    PNODE head = NULL;
+
+    InsertFirst(&head,10);
+    InsertFirst(&head,21);
+    InsertFirst(&head,51);
+    InsertFirst(&head,34);
+    InsertLast(&head,101);
+    InsertLast(&head,102);
+    InsertLast(&head,21);
+    InsertLast(&head,103);
+
+    Display(head);
+    
+    iRet = FirstOccurrence(head,21);
+
+    printf("Position of first occurrence : %d",iRet);
+
+    return 0;
+}
